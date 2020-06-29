1c6287bab70c33fa16f0d173d4d38eef11192262
JSkrat <jskrat@gmail.com>
2020-06-29 16:40:41 +0300

    reconfigure gpio under raspberry

--- DevHub/main.go
+++ DevHub/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	model := RFModel.Init(nRF_model.TransmitterSettings{
-		PortName: "",
-		IrqName:  "7",
-		CEName:   "2",
+	model := RFModel.RFModel{}
+	RFModel.Init(&model, nRF_model.TransmitterSettings{
+		PortName: "/dev/spidev0.0",
+		IrqName:  "25",
+		CEName:   "24",
 	})
+	defer RFModel.Close(&model)
 	uid := RFModel.UID{Address: [5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0x54}, Unit: 1}
 	RFModel.WriteFunction(&model, uid, 0x19, byte(0xE1))
 	response := RFModel.ReadFunction(&model, uid, 0x18).(byte)
 	fmt.Printf("Wrote 0xE1, read %v", response)
-	defer RFModel.Close(&model)
-
 }
